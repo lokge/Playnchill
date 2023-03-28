8790e9e91681905748ea98fee032061d052e8e61
lokge <molnia.erkin@gmail.com>
2023-03-29 00:27:10 +0600

    product started

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,11 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import axios from "../../utils/axios";
 import {useParams} from "react-router-dom";
+import {AiOutlineHeart} from "react-icons/ai"
+import {CustomContext} from "../../utils/Context";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
 
 const Product = () => {
 
     const [product, setProduct] = useState([])
-    const {id} = useParams()
+    const {id, category} = useParams()
+
+    const {addBasket} = useContext(CustomContext)
 
     useEffect(() => {
         axios(`http://localhost:6969/products/${id}`)
@@ -20,12 +26,75 @@ const Product = () => {
     }
     return (
         <section className="product">
-            <div className="container">
-                <div className="product__main">
-                    <div className="product__block">
-                        <img src={product.image} alt={product.title} className="product__main-img"/>
+            <div className="product__main">
+                <div className="container">
+                    <div className="product__container">
+                        <div className="product__main-imgCont">
+                            <img src={product.image} alt={product.title} className="product__main-img"/>
+                        </div>
+                        <div className="product__block">
+                            <h3 className="product__title">{product.title}</h3>
+                            <span className="product__availability">{product.availability === "true"
+                                ? <div className="product__check">
+                                    <span style={{background: "#01CB62"}} className="product__checked"></span> <p className="product__true">В наличии</p>
+                                </div>
+                                : "Не в наличии"}
+                            </span>
+                            <div className="product__prices">
+                                <p className="product__price">{product.price}</p>
+                                <p className="product__discount">{product.discount}</p>
+                                <p className="product__original">{product.price}</p>
+                            </div>
+                            <div className="product__btns">
+                                <button className="product__btn">Купить</button>
+                                <button type='button' onClick={() => addBasket(product)} className="product__btn">В корзину</button>
+                                <button className="product__btn"><AiOutlineHeart className="product__btn-icon"/></button>
+                            </div>
+                            <div className="product__description">
+                                <span className="product__description-text">
+                                    <p className="product__description-genre">Жанр</p>
+                                    <p className="product__description-ans">{product.category}</p>
+                                </span>
+                                <span className="product__description-text">
+                                    <p className="product__description-genre">Платформа</p>
+                                    <p className="product__description-ans">{product.service}</p>
+                                </span>
+                                <span className="product__description-text">
+                                    <p className="product__description-genre">Регион активации</p>
+                                    <p className="product__description-ans">{product.region}</p>
+                                </span>
+                                <span className="product__description-text">
+                                    <p className="product__description-genre">Тип товара</p>
+                                    <p className="product__description-ans">{product.contentType}</p>
+                                </span>
+                            </div>
+                        </div>
                     </div>
-                    <div className="product__block"></div>
+                    <a
+                        data-fancybox="gallery"
+                        data-src="https://lipsum.app/id/2/1600x1200"
+                        data-caption="Optional caption,&lt;br /&gt;that can contain &lt;em&gt;HTML&lt;/em&gt; code"
+                    >
+                        <img src="https://lipsum.app/id/2/200x150" width="200" height="150" alt="" />
+                    </a>
+
+                    <a data-fancybox="gallery" data-src="https://lipsum.app/id/3/1600x1200">
+                        <img src="https://lipsum.app/id/3/200x150" width="200" height="150" alt="" />
+                    </a>
+
+                    <a data-fancybox="gallery" data-src="https://lipsum.app/id/4/1600x1200">
+                        <img src="https://lipsum.app/id/4/200x150" width="200" height="150" alt="" />
+                    </a>
+                </div>
+            </div>
+
+            <div className="product__descriptions">
+                <div className="container">
+                    <div className="product__header"></div>
+                    <div className="product__about">
+                        <h5 className="product__about-title">О чем {product.title}?</h5>
+                        <p className="product__about-text">{product.description}</p>
+                    </div>
                 </div>
             </div>
         </section>
